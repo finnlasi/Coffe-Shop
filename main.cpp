1a2487af9d40dad78c2d55d16a14c768e79da5d7
finnlasi <haniffaisal2003@gmail.com>
2025-06-08 00:27:02 +0800

    Mocha
    0 to stop loop, fixed where the code only accept 1 order

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include<iomanip>
 using namespace std;
 struct Coffee {
     string name;
@@ -14,52 +15,63 @@ void setCoffee(Coffee &c, string n, double p) {
     c.price = p;
 }
 void initializecoffes(Coffee coffees[]){
-    setCoffee(coffees[0], "Caffe Latte", 10.50);
+    setCoffee(coffees[0], "Caffe Latte", 9.50);
     setCoffee(coffees[1], "Espresso", 7.00);
-    setCoffee(coffees[2], "Cappuccino", 9.00);
-    setCoffee(coffees[3], "Americano", 8.50);
+    setCoffee(coffees[2], "Cappuccino", 8.00);
+    setCoffee(coffees[3], "Americano", 7.50);
+    setCoffee(coffees[4], "Mocha", 8.50);
 }
 void coffelist(Coffee coffees[], int size){
-    for(int i = 0;i<4;i++){
-        cout << i+1 << ". " << coffees[i].name<<"\t" << " - RM" << coffees[i].price << endl;
+    for(int i = 0;i<5;i++){
+        cout << i+1 << ". " << coffees[i].name<<"\t" << " - RM " << coffees[i].price
+        <<fixed<<setprecision(2)<< endl<< endl;
     }
     
 }
 void addToCart(vector<Coffee> &cart, Coffee coffees[], int size) {
     int index;
-    cout << "Enter product index to add to cart(enter 0 to stop adding): ";
-    cin >> index;
-
-    do{
-    if (index >= 1 && index <= size) {
-        cart.push_back(coffees[index - 1]);
-        cout << coffees[index - 1].name << " added to cart!\n";
-    } else {
-        cout << "Invalid index!\n";
-    }
-    }while(index != 0);
+    do {
+        cout << "Enter product index to add to cart (0 to stop): ";
+        cin >> index;
+        if (index == 0) {
+            cout << "Stopping...\n";
+            break;
+        }
+        if (index >= 1 && index <= size) {
+            cart.push_back(coffees[index - 1]);
+            cout << coffees[index - 1].name << " added to cart!\n";
+        } else {
+            cout << "Invalid index! Try again.\n";
+        }
+    } while (true);
 }
 
 // Print cart contents
 void printCart(const vector<Coffee> &cart) {
     cout << "\nYour Cart:\n";
+    float total=0;
     if (cart.empty()) {
         cout << "Cart is empty.\n";
     } else {
         for (int i = 0; i < cart.size(); i++) {
             cout << i + 1 << ". " << cart[i].name
-                 << " - RM" << cart[i].price << endl;
+                 << " - RM " << cart[i].price <<fixed<<setprecision(2)<< endl;
+                 
+                 total=total + cart[i].price;
         }
+        cout<<"\nYour total is RM "<<total<<fixed<<setprecision(2)<< endl;
+
     }
 }
 int main(){
-    Coffee coffees[4];
+    Coffee coffees[5];
     initializecoffes(coffees);
-    coffelist(coffees,4);
+    coffelist(coffees,5);
     
     vector<Coffee> cart;
 
-    addToCart(cart, coffees,4);
+    addToCart(cart, coffees,5);
+    printCart(cart);
     
     return 0;
 }
